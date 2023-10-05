5e0dc7df117da4fa47d92608a25c352b156a1dfa
Tomáš Černý <tmscer@gmail.com>
2023-10-05 17:27:05 +0200

    Add proxy connection example to playground

--- playground/main.tf
+++ playground/main.tf
@@ -10,10 +10,37 @@ provider "remote" {
   }
 }
 
+provider "remote" {
+  alias = "proxied"
+
+  conn {
+    host     = "remotehost2"
+    port     = 22
+    user     = "root"
+    password = "password"
+    sudo     = false
+  }
+
+  proxy_conn {
+    host     = "localhost"
+    port     = 8022
+    user     = "root"
+    password = "password"
+    sudo     = false
+  }
+}
+
 resource "remote_file" "foo" {
   path        = "/tmp/.bashrc"
   content     = "alias ll='ls -alF'"
   permissions = "0644"
   owner       = "0"
   group       = "0"
+}
+
+resource "remote_file" "bar" {
+  provider = remote.proxied
+
+  path    = "/tmp/hello.txt"
+  content = "Hello, World!"
 }
